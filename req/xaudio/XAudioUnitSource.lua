cb5f441e0677838082c698e9411de8ed6be5a9ef
Campbell Suter <znix@znix.xyz>
2017-12-09 14:08:39 +1300

    Fix crashes with XAudio UnitSource

--- req/xaudio/XAudioUnitSource.lua
+++ req/xaudio/XAudioUnitSource.lua
@@ -9,9 +9,18 @@ end
 function C:update(...)
 	self.super.update(self, ...)
 
+	-- If we were just closed, or the unit is dead, do nothing
+	if self:is_closed() then return end
+
 	local unit = self._unit
 	if self._unit == XAudio.PLAYER then
 		unit = XAudio._player_unit
+	end
+
+	-- If the unit is dead, we're done.
+	if not alive(unit) then
+		self:close()
+		return
 	end
 
 	local pos = unit:position()
