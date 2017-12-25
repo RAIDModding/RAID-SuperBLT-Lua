485465f637aa039bf9eac9ae43af296bfcf0ea2f
Campbell Suter <znix@znix.xyz>
2017-12-25 15:41:06 +1300

    Move temporary directory for mod downloads to the downloads folder

--- req/BLTDownloadManager.lua
+++ req/BLTDownloadManager.lua
@@ -126,7 +126,12 @@ function BLTDownloadManager:clbk_download_finished( data, http_id )
 			end
 		end
 
-		local temp_dir = Application:nice_path( download.update:GetInstallDirectory() .. "_temp" )
+		local install_dir = download.update:GetInstallDirectory()
+		local temp_dir = Application:nice_path( install_dir .. "_temp" )
+		if install_dir == BLTModManager.Constants:ModsDirectory() then
+			temp_dir = Application:nice_path( BLTModManager.Constants:DownloadsDirectory() .. "_temp" )
+		end
+
 		local file_path = Application:nice_path( BLTModManager.Constants:DownloadsDirectory() .. tostring(download.update:GetId()) .. ".zip" )
 		local temp_install_dir = Application:nice_path( temp_dir .. "/" .. download.update:GetInstallFolder() )
 		local install_path = Application:nice_path( download.update:GetInstallDirectory() .. download.update:GetInstallFolder() )
