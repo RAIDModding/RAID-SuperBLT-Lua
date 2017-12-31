2d5ab4eb46848c296fdf6af181b918f01183a535
Campbell Suter <znix@znix.xyz>
2017-12-31 17:28:56 +1300

    Seperate the SuperMod asset loader away from the supermod class

--- req/supermod/BLTSuperMod.lua
+++ req/supermod/BLTSuperMod.lua
@@ -1,11 +1,7 @@
 
 BLTSuperMod = blt_class()
-BLTSuperMod._dynamic_unloaded_assets = {}
 
-BLTSuperMod.DYNAMIC_LOAD_TYPES = {
-	unit = true,
-	effect = true
-}
+BLT:Require("req/supermod/SuperModAssetLoader")
 
 function BLTSuperMod.try_load(mod, file_name)
 	local supermod_path = "mods/" .. mod:GetId() .. "/" .. (file_name or "supermod.xml")
@@ -32,102 +28,21 @@ end
 
 function BLTSuperMod:new(mod, xml)
 	self._mod = mod
+	self._assets = self.AssetLoader:new(self)
 
 	self:_replace_includes(xml)
 
 	self:_load_xml(xml, {
-		base_path = ""
+		base_path = mod:GetPath()
 	})
 end
 
-function BLTSuperMod:_refine_scope(xml, scope)
-	local new_scope = {}
-	setmetatable(new_scope, {__index = scope})
-
-	for name, val in pairs(xml.params) do
-		new_scope[name] = val
-	end
-
-	return new_scope
-end
-
-function BLTSuperMod:_load_xml(xml, scope)
-	for _, tag in ipairs(xml) do
-		local lscope = self:_refine_scope(xml, scope)
-		if tag.name == "group" then
-			self:_load_xml(tag, lscope)
-		elseif tag.name == "assets" then
-			self:_load_assets(tag, lscope)
-		else
-			error("Unknown tag name " .. tag.name .. " in root :" .. tag._doc.filename)
-		end
-	end
-end
-
-function BLTSuperMod:_load_assets(xml, scope)
-	for _, tag in ipairs(xml) do
-		local lscope = self:_refine_scope(xml, scope)
-		if tag.name == "group" then
-			self:_load_assets(tag, lscope)
-		elseif tag.name == "file" then
-			local name = tag.params.name
-			local path = tag.params.path or (self._mod:GetPath() .. lscope.base_path .. name)
-			self:_load_asset(name, path, lscope)
-		else
-			error("Unknown tag name " .. tag.name .. " in <assets> :" .. tag._doc.filename)
-		end
-	end
-end
-
-local function _flush_assets(dres)
-	dres = dres or (managers and managers.dyn_resource)
-	if not dres then return end
-
-	local next_to_load = {}
-
-	local i = 1
-	for _, asset in pairs(BLTSuperMod._dynamic_unloaded_assets) do
-		local ext = Idstring(asset.extension)
-		local dbpath = Idstring(asset.dbpath)
-
-		--log("Loading " .. asset.dbpath .. " " .. asset.extension .. " from " .. asset.file)
-
-		DB:create_entry(ext, dbpath, asset.file)
-
-		if asset.dyn_package then
-			dres:load(ext, dbpath, dres.DYN_RESOURCES_PACKAGE, function()
-				-- This is called when the asset is done loading.
-				-- Should we wait for these to all be called?
-			end)
-
-			i = i + 1
-		end
-	end
-
-	BLTSuperMod._dynamic_unloaded_assets = {}
-end
-Hooks:Add("DynamicResourceManagerCreated", "BLTAssets.DynamicResourceManagerCreated", _flush_assets)
-
-function BLTSuperMod:_load_asset(name, file, params)
-	local dot_index = name:find(".", 1, true)
-	local dbpath = name:sub(1, dot_index - 1)
-	local extension = name:sub(dot_index + 1)
-
-	local dyn_package = BLTSuperMod.DYNAMIC_LOAD_TYPES[extension] or false
-	if params.dyn_package == "true" then
-		dyn_package = true
-	elseif params.dyn_package == "false" then
-		dyn_package = false
-	end
-
-	table.insert(BLTSuperMod._dynamic_unloaded_assets, {
-		dbpath = dbpath,
-		extension = extension,
-		file = file,
-		dyn_package = dyn_package
+function BLTSuperMod:_load_xml(xml, parent_scope)
+	BLTSuperMod._recurse_xml(xml, parent_scope, {
+		assets = function(tag, scope)
+			self._assets:FromXML(tag, scope)
+		end,
 	})
-
-	_flush_assets()
 end
 
 function BLTSuperMod:_replace_includes(xml)
@@ -158,5 +73,29 @@ function BLTSuperMod:_replace_includes(xml)
 		end
 
 		self:_replace_includes(tag)
+	end
+end
+
+function BLTSuperMod._recurse_xml(xml, parent_scope, callbacks)
+	for _, tag in ipairs(xml) do
+		local scope = {}
+		setmetatable(scope, {__index = parent_scope})
+
+		for name, val in pairs(tag.params) do
+			if name:sub(1,1) == ":" then
+				name = name:sub(2)
+				scope[name] = scope[name] .. val
+			else
+				scope[name] = val
+			end
+		end
+
+		if tag.name == "group" then
+			BLTSuperMod._recurse_xml(tag, scope, callbacks)
+		elseif callbacks[tag.name] then
+			callbacks[tag.name](tag, scope, callbacks)
+		else
+			error("Unknown tag name " .. tag.name .. " in:" .. tag._doc.filename)
+		end
 	end
 end
