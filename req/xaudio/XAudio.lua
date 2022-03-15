b14b1a95e24a9c9373fb400882fe721954de7c41
Dom <11101723-Dom3062@users.noreply.gitlab.com>
2022-03-15 19:52:33 +0000

    Optimize XAudio update loop

--- req/xaudio/XAudio.lua
+++ req/xaudio/XAudio.lua
@@ -6,9 +6,13 @@ XAudio.PLAYER = "xa_player_1"
 -- Variables
 XAudio._sources = {}
 XAudio._next_source_id = 1
+-- Vanilla's default value for Music and Sound is 20%
+-- If the loaded value is higher or lower than 20%, it is updated in the callback down below
+-- The value is updated in menu/mission load (if not 20% on first game load, everytime else)
+-- or user changes the value themselves (always regardless of value)
 XAudio._base_gains = {
-	sfx = 1,
-	music = 1
+	sfx = 0.2,
+	music = 0.2
 }
 
 BLT:Require("req/xaudio/XAudioBuffer")
@@ -20,17 +24,14 @@ BLT:Require("req/xaudio/VoicelineManager")
 -- This is our wu-to-meters conversion
 -- You can get it using blt.xaudio.getworldscale() if you need to use it
 -- This means we can use positions from the game without worrying about
---  unit conversion or anything.
+-- unit conversion or anything.
 blt.xaudio.setworldscale(100)
 
 -- Delete any existing sources from the last heist/menu
 blt.xaudio.reset()
 
 local function update(t, dt, paused)
-	XAudio._base_gains.music = managers.user:get_setting("music_volume") / 100
-	XAudio._base_gains.sfx = managers.user:get_setting("sfx_volume") / 100
-
-	for id, src in pairs(XAudio._sources) do
+	for _, src in pairs(XAudio._sources) do
 		src:update(t, dt, paused)
 	end
 end
@@ -45,4 +46,13 @@ end)
 
 Hooks:Add("GameSetupPausedUpdate", "Base_XAudio_GameSetupPausedUpdate", function(t, dt)
 	update(t, dt, true)
+end)
+
+Hooks:Add("MenuManagerInitialize", "Base_XAudio_SetUpdateCallbacks", function(menu_manager)
+	managers.user:add_setting_changed_callback("music_volume", function(setting_name, default_value, value)
+		XAudio._base_gains.music = value / 100
+	end, true)
+	managers.user:add_setting_changed_callback("sfx_volume", function(setting_name, default_value, value)
+		XAudio._base_gains.sfx = value / 100
+	end, true)
 end)
