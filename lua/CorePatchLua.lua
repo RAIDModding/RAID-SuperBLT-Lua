51e83809c42c9503d5b5358fe68c002e7abd2367
Hoppip <segabl91@gmail.com>
2024-03-19 12:55:21 +0100

    Simplify CorePatchLua.lua

--- lua/CorePatchLua.lua
+++ lua/CorePatchLua.lua
@@ -1,21 +1,7 @@
+-- core/lib/system/corepatchlua adds a metatable to _G that prevents creating and checking variables in _G that have not been created by the game
+-- We simply remove the offending metatable functions here
 local mt = getmetatable(_G)
-if mt == nil then
-	mt = {}
-	setmetatable(_G, mt)
-end
-mt.__declared = {}
-function mt.__newindex(t, n, v)
-	if not mt.__declared[n] then
-		mt.__declared[n] = true
-	end
-	rawset(t, n, v)
-end
-
-function mt.__index(t, n)
-	if not mt.__declared[n] then
-		local info = debug.getinfo(2, "S")
-		if info and info.what ~= "main" and info.what ~= "C" then
-			return nil
-		end
-	end
+if mt then
+	mt.__newindex = nil
+	mt.__index = nil
 end
