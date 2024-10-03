a57b9bbca22451f7ed7d1e1f2ed5b463ef00c2d1
BangL <henno.rickowski@gmail.com>
2024-10-03 18:51:41 +0200

    Revert "remove xaudio"
    This reverts commit 61d7dca7552f3c16898587b145d9f0160ce3bcdd.

--- base.lua
+++ base.lua
@@ -54,6 +54,7 @@ BLT:Require("req/BLTNotificationsManager")
 BLT:Require("req/BLTPersistScripts")
 BLT:Require("req/BLTKeybindsManager")
 BLT:Require("req/BLTAssetManager")
+BLT:Require("req/xaudio/XAudio")
 
 ---Writes a message to the log file
 ---Multiple arguments can be passed to the function and will be concatenated
