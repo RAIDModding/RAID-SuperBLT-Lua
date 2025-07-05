82677c093bf004f07518a2c17301bc4adc00cb6b
BangL <henno.rickowski@gmail.com>
2025-07-06 01:07:04 +0200

    fix ChatManagerOnReceiveMessage

--- lua/ChatManager.lua
+++ lua/ChatManager.lua
@@ -1,9 +1,13 @@
 Hooks:RegisterHook("ChatManagerOnSendMessage")
-Hooks:PreHook(ChatManager, "send_message", "BLT.ChatManager.send_message", function(self, channel_id, sender, message)
-	Hooks:Call("ChatManagerOnSendMessage", channel_id, sender, message)
-end)
+Hooks:PreHook(ChatManager, "send_message", "BLT.ChatManager.send_message",
+	function(self, channel_id, sender, message)
+		Hooks:Call("ChatManagerOnSendMessage", channel_id, sender, message)
+	end
+)
 
 Hooks:RegisterHook("ChatManagerOnReceiveMessage")
-Hooks:PreHook(ChatManager, "_receive_message", "BLT.ChatManager._receive_message", function(self, channel_id, name, message, color, icon)
-	Hooks:Call("ChatManagerOnReceiveMessage", channel_id, name, message, color, icon)
-end)
+Hooks:PreHook(ChatManager, "_receive_message", "BLT.ChatManager._receive_message",
+	function(self, channel_id, name, peer_id, message, color, icon, system_message)
+		Hooks:Call("ChatManagerOnReceiveMessage", channel_id, name, message, color, icon)
+	end
+)
